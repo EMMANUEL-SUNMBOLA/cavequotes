c526a14b94807dedff0d30996c8ff302b53855b6
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-11-23 10:46:58 +0100

    mobile responsiveness incoming

--- main.js
+++ main.js
@@ -22,6 +22,23 @@ async function fetchQuote(){
     for(let i = 0; i < splitContent.length; i++){
         text += splitContent[i] + "%20";
     }
-    text = 'https://x.com/intent/tweet?text=' + text;
-    tweetLink.setAttribute('href', text);
+    text = 'https://x.com/intent/tweet?text=' + text + '&hashtags=CaveQuotes,Wisdom&url=';
+    tweetLink.setAttribute('href', text); 
 }
+
+const copy = ()=>{
+        /* Get the text field */
+        var copyText = document.querySelector(".quote");
+      
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+      
+         /* Copy the text inside the text field */
+        navigator.clipboard.writeText(copyText.value);
+      
+        /* Alert the copied text */
+        alert("Copied the text: " + copyText.value);
+}
+
+// https://twitter.com/intent/tweet?text=Yesterday%20is%20history,%20tomorrow%20is%20a%20mystery,%20today%20is%20God%27s%20gift,%20that%27s%20why%20we%20call%20it%20the%20present.&hashtags=DIQs,Wisdom&url=http://bit.ly/amazing-quotes
