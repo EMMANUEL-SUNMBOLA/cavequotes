19192745d05a3ef85bc8da6a0a4216b3bde1d7cf
Babatunde Toluwalase <Toluwalase@Teazer-MacBook-Pro.local>
2023-11-26 13:41:10 +0100

    fixed screenshot function

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import html2canvas from "./node_modules/html2canvas/dist/html2canvas.esm.js";
+
+
 const themeToggle = ()=>{
     let body = document.querySelector('#body');
     let icon = document.querySelector('.fa-solid');
@@ -50,9 +53,22 @@ const toggleVis = ()=>{
     event.preventDefault();
 }
 
-function takeImg(){
-    let image = document.querySelector('.quote');
-    html2canvas(image).then(canvas => {
-        document.body.appendChild(canvas)
+// parameterized function takeImage to make it more reusable
+
+export function takeImg(elem, wrapper=document.body){
+    console.log("trying to take image");
+    html2canvas(elem).then(canvas => {
+        wrapper.innerHTML = null
+        wrapper.appendChild(canvas)
+        console.log(screenshotWrapper);
+
     })
 }
+// redifined image to globalscope
+const  image = document.querySelector('.quoteDiv');
+const cammera = document.querySelector("#camera")
+const screenshotWrapper = document.querySelector("#screenshot")
+// adding a click event to camera
+cammera.addEventListener("click", () => takeImg(image, screenshotWrapper))
+
+
